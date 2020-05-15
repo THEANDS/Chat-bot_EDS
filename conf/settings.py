e3781f54ee52012a59b59ee9affb566d77230304
THEANDS <andersonvartam@gmail.com>
2020-05-15 20:33:05 -0300

    second tentative

---
+++ conf/settings.py
@@ -0,0 +1,3 @@
+bot_token = "1209887550:AAE5QFLAG0vlgDRvJgNbNhhimlHo8e4Au7E"
+bot_user_name = "AlfredP_bot"
+URL = "https://edschatbot.herokuapp.com/"
---
+++ core.py
@@ -0,0 +1,42 @@
+from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters	
+from conf.settings import bot_token,bot_user_name, URL
+from flask import Flask
+
+global TOKEN
+
+
+TOKEN = bot_token
+
+updater = Updater(token= TOKEN,use_context=True)
+dispatcher = updater.dispatcher
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN),methods=['POST'])
+def welcome(update, context):
+	for member in update.message.new_chat_members:
+		update.message.reply_text("{username} Welcome to the party!!\U0001F47E\nse apresente por favor e não esqueça de entrar no grupo de avisos:t.me/avisosEDS".format(username=member.username))
+	return 'ok'
+
+@app.route('/setWebhook',methods=['GET','POST'])
+def set_webhook():
+	s = updater.bot.setWebhook('{URL}{HOOK}'.format(URL=URL,HOOK=TOKEN))
+
+	if s:
+		return "webhook setup ok"
+	else:
+		return "webhook setup failed"
+
+
+welcome_handler = MessageHandler(Filters.status_update.new_chat_members,welcome)
+dispatcher.add_handler(welcome_handler)
+
+updater.start_polling()
+
+@app.route('/')
+def index():
+	return '.'
+
+
+if __name__ == '__main__':
+	app.run(use_reloader=True)
