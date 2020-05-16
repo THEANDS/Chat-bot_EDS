4cefc6bb1b7d1be1475024cdd257dbcfd2d516c2
THEANDS <andersonvartam@gmail.com>
2020-05-16 20:30:00 -0300

    add news funcs

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters	
 from conf.settings import bot_token,bot_user_name, URL
 from flask import Flask
-from comandos import comandos
+
+
 global TOKEN
 
 
@@ -13,7 +14,7 @@ dispatcher = updater.dispatcher
 
 app = Flask(__name__)
 
-@app.route('/{}'.format(TOKEN),methods=['POST'])
+
 def welcome(update, context):
 	for member in update.message.new_chat_members:
 		if member.username == 'AlfredP_bot':
@@ -21,6 +22,10 @@ def welcome(update, context):
 		else:		
 			update.message.reply_text("{username} Welcome to the party!!\U0001F47E\nse apresente por favor e não esqueça de entrar no grupo de avisos:t.me/avisosEDS".format(username=member.username))
 	return 'ok'
+
+def get_admin_ids(bot,chat_id):
+	return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
+
 
 
 @app.route('/setwebhook',methods=['GET','POST'])
@@ -33,17 +38,15 @@ def set_webhook():
 		return "webhook setup failed"
 
 
-
-welcome_handler = MessageHandler(Filters.status_update.new_chat_members,welcome)
-dispatcher.add_handler(welcome_handler)
-
-updater.start_polling()
-
-
 @app.route('/')
 def index():
 	return 'index'
 
 if __name__ == '__main__':
+	welcome_handler = MessageHandler(Filters.status_update.new_chat_members,welcome)
+	dispatcher.add_handler(welcome_handler)
+
+	updater.start_polling()
+
 	port = int(os.environ.get("PORT",5000))
 	app.run(host='0.0.0.0',port=port)
